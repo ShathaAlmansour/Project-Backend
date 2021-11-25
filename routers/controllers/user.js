978b8e8bdeef3f258c413a47366c75ac08a0ff2f
ShathaAlmansour <>
2021-11-25 23:13:00 +0300

    added

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -29,5 +29,89 @@ const getAlluser = (req, res) => {
       res.send(err);
     });
 };
+const update = (req, res) => {
+  const { id } = req.params;
+  const { name, password, email } = req.body;
+  userModel
+    .findOneAndUpdate({ _id: id }, { name, password, email }, { new: true })
+    .exec()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
-module.exports = { creatUser, getAlluser };
+
+
+const removeUserCart = (req, res) => {
+  const { email, _id } = req.params;
+  userModel
+    .findOneAndUpdate(
+      { email: email },
+      { $pull: { favoriteSchema: _id } },
+      { new: true }
+    )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+const cartUsercheck = (req, res) => {
+  const { email, ObjectId } = req.params;
+  console.log(email, ObjectId);
+  userModel.findOne({ ObjectId: req.params.ObjectId }).then((user) => {
+    userModel
+      .findOneAndUpdate(
+        { email: email },
+        { $push: { favoriteSchema: ObjectId } },
+        { new: true }
+      )
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+};
+const getCart = (req, res) => {
+  const { email } = req.params;
+  userModel
+    .find({ email: email })
+    .populate("favoriteSchema")
+    .exec()
+    .then((result) => {
+      res.send(result[0].favoriteSchema);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+const remove = (req, res) => {
+  const { id } = req.params;
+  const { name, password, email } = req.body;
+  userModel
+    .findOneAndRemove({ _id: id }, { new: true })
+    .exec()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+module.exports = { 
+  creatUser, 
+  getAlluser,
+  update,
+  removeUserCart,
+  getCart,
+  remove,
+  cartUsercheck,
+
+};
--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { creatUser, getAlluser } = require("../controllers/user");
-
+const { creatUser, getAlluser, update,cartUsercheck,removeUserCart,getCart } = require("../controllers/user");
 const userRouter = express.Router();
 
 userRouter.post("/create", creatUser);
 userRouter.get("/", getAlluser);
+userRouter.put("/update/:id", update);
+userRouter.put("/yourcart/:email/:ObjectId", cartUsercheck);
+userRouter.put("/removecart/:email/:_id", removeUserCart);
+userRouter.get("/cart/:email", getCart);
+
 module.exports = userRouter;
